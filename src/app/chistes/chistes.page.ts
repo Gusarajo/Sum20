b35008f9b18c105e04d9baaa4bce6054822d3ff0
Donvicio <10143@portalcedi.edu.mx>
2025-03-18 10:41:21 -0600

    crud

--- src/app/chistes/chistes.page.ts
+++ src/app/chistes/chistes.page.ts
@@ -1,20 +1,137 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol, IonItem } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { BaseDatosService, Task } from '../task.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chistes',
   templateUrl: './chistes.page.html',
   styleUrls: ['./chistes.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonGrid, IonRow, IonCol, IonItem]
 })
 export class ChistesPage implements OnInit {
+  constructor(
+    private alertController: AlertController,
+    public reviewService: BaseDatosService,
+    private router: Router,
+    private authService: AuthService,
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+    this.tasks$ = this.reviewService.getTasks();
+
+  }
+  ChisteInput: string = "";
+  TipoInput: string = "";
+  ExplicacionInput: string = "";
+  tasks$: Observable<Task[]> = new Observable<Task[]>();
+
+  async onSubmit() {
+    if (this.ChisteInput && this.TipoInput && this.ExplicacionInput) {
+      const newTask: Task = {
+        Chiste: this.ChisteInput,
+        Tipo: this.TipoInput,
+        Explicacion: this.ExplicacionInput,
+      }
+      await this.reviewService.addTask(newTask);
+      this.ChisteInput = "";
+      this.TipoInput = "";
+      this.ExplicacionInput = "";
+
+      const alert = await this.alertController.create({
+        header: 'Review agregada',
+        message: 'Tu review se agrego',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'ERROR',
+        message: 'Tu review no se pudo agregar',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
   }
 
+  borrar(id: string) {
+    this.reviewService.deleteTask(id);
+    const alert = this.alertController.create({
+      header: 'review fue borrada',
+      message: 'Tu review se borro',
+      buttons: ['OK'],
+    });
+  }
+
+  async Editar(id: string, task: Task) {
+    const alert = await this.alertController.create({
+      header: 'Editar Review',
+      inputs: [
+        {
+          name: 'Chiste',
+          type: 'text',
+          placeholder: 'Chiste',
+          value: task.Chiste
+        },
+        {
+          name: 'Tipo',
+          type: 'text',
+          placeholder: 'Tipo',
+          value: task.Tipo
+        },
+        {
+          name: 'Explicacion',
+          type: 'text',
+          placeholder: 'Opinion',
+          value: task.Explicacion
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Guardar',
+          handler: (data) => {
+            if (data.Chiste && data.Tipo && data.Explicacion && data.calificacion) {
+              const updatedTask: Partial<Task> = {
+                Chiste: data.Chiste,
+                Tipo: data.Tipo,
+                Explicacion: data.Explicacion,
+              };
+              this.reviewService.updateTask(id, updatedTask);
+              this.showAlert('Review actualizada', 'La review ha sido actualizada correctamente.');
+            } else {
+              this.showAlert('Error', 'Todos los campos son obligatorios.');
+            }
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
+  
+  private async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+  
+    await alert.present();
+  }
+  onSignUp() {
+    this.authService.logout();
+    this.router.navigateByUrl("sign");
+  }
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc, updateDoc, deleteDoc, doc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+export interface Task {
+  id?: string;
+  Chiste: string;
+  Tipo: string;
+  Explicacion: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService {
+export class BaseDatosService {
+  private tasksCollection = collection(this.firestore, 'tasks');
 
-  constructor() { }
+  constructor(private firestore: Firestore) { }
+
+  getTasks(): Observable<Task[]> {
+    return collectionData(this.tasksCollection, { idField: 'id' }) as Observable<Task[]>;
+  }
+  addTask(task: Task) {
+    return addDoc(this.tasksCollection, task);
+  }
+  updateTask(id: string, data: Partial<Task>) {
+    const taskDoc = doc(this.firestore, `tasks/${id}`);
+    return updateDoc(taskDoc, data);
+  }
+  deleteTask(id: string) {
+    const taskDoc = doc(this.firestore, `tasks/${id}`);
+    return deleteDoc(taskDoc);
+  }
 }
